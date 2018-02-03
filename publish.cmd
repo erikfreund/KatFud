cd36573060d98d7cf3de00b8e496b6fc4728a15f
erikfreund <erik.freund@gmail.com>
2018-02-03 10:35:01 -0800

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=katfud-Web-Deploy.pubxml -p:Password=7MlQzfP9ugoBM4tWoqxoxPTAdLdLc6QzbgPGuktBgoydA0Zo4Ex0cwE1Nk3y
+
